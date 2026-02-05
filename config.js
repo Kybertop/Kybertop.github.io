f198a792f8991383253223ad91189f10c2b1a395
Kybertop <Kybertop505@gmail.com>
2026-02-05 11:09:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Konfigurácia pre Google Sheets API
 const CONFIG = {
     // Google Sheets Web App URL
-    API_URL: 'https://script.google.com/macros/s/AKfycbxVQT4ooTvoQZ2XffHWVaKKCbIX6IM_tjqfUjCeuEUyEk2ceKmmPsEyjnHPiQr4ORE/exec'
+    API_URL: 'https://script.google.com/macros/s/AKfycbxDuXPipilIHKjc3gcK05HEwPA84ZhszHA5DCng2C_r9SvitKs78jVHbMD3I6h1C-U/exec'
 };
